d00bb71b677f9e464a0c4c3deefae4e953be97cd
Olga Fedorenko <olga@synapsefi.com>
2020-04-27 15:23:29 -0700

    adding create a new user every time for demo

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,13 @@
 import React, { Component, Fragment } from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
 import { connect } from 'react-redux';
-import { Button } from 'semantic-ui-react';
 
 import Header from './Header';
-import AccountList from './AccountList';
-import Logo from '../svg/Logo';
 import { updateNewNodes, updateUserInfo, updateBanner } from '../actions/bankLoginActions';
 import { createTestUser, generateOauthKey, generatePublicKey, getUserInfo } from '../services/nodeService';
 import { userAction, agentAction } from '../constants/actionsList';
 import displayErrorBanner from '../services/displayErrorBanner';
 import ToggleBanner from './Banner';
 
-const bank = 'https://synapse-chatbot-demo.s3.amazonaws.com/assets/bank.gif';
 const agentDemoGif = 'https://synapse-banking-qa.s3-us-west-1.amazonaws.com/Agent_Demo.gif';
 const userDemoGif = 'https://synapse-banking-qa.s3-us-west-1.amazonaws.com/End_user_demo.gif';
 class Main extends Component {
@@ -27,7 +21,6 @@ class Main extends Component {
       publicKey: 'public_key_qWCwNJcVPT2jMY105s7K6bUDm3gixoXkf94ZrR8F'
     };
     const receiveMessage = (e) => {
-      // console.log(props);
     };
     window.addEventListener('message', receiveMessage, false);
   }
@@ -35,12 +28,8 @@ class Main extends Component {
 
   componentDidMount = () => {
     localStorage.clear();
-    // this.createNewUser();
+    this.createNewUser();
     this.internetCheck();
-    // this.getPublicKey();
-    this.updateUserInfo();
-    // this.pushToIframe();
-    // // this.getNodes();
   }
 
   internetCheck = () => {
@@ -66,29 +55,11 @@ class Main extends Component {
 
   pushToIframe = (refreshTocken) => {
     const { id, refreshToken, fp } = this.state;
-    // var updatePublicKey = function () {
-    //   return 'public_key_qWCwNJcVPT2jMY105s7K6bUDm3gixoXkf94ZrR8F';
-    // }
-    // var updateOauthKey = function () {
-    //   return 'oauth_zoVHICyq7Xl8nBYmaxdMUTibsApJ50Rc0QGgWkN4';
-    // }
-    // var updateUserId = function () {
-    //   return '5e692c033c4e28008cee2f07';
-    // }
-    // const updatePublicKey = () => 'public_key_qWCwNJcVPT2jMY105s7K6bUDm3gixoXkf94ZrR8F';
-    // const updateOauthKey = () => 'oauth_snMDtxJzhaLR13BIEAFNSiqjdXkYZ0uvowpyrKC4';
-    const updateUserId = () => '5e3c756e3c4e28008d53f68e';
-    // const updateUserId = () => '5cdca3d814ddee0064a05b17';
     const updatePublicKey = async () => {
       const publicKey = await this.getPublicKey();
-      // this.setState({ publicKey });
       return publicKey;
     };
-    // const updateUserId = () => id;
-    // const updateOauthKey = async () => {
-    //   const oauth = await this.generateOuth(refreshTocken);
-    //   return oauth;
-    // };
+    const updateUserId = () => id;
     const updateFingerprint = () => fp;
 
     window.SynapseMain({ updatePublicKey, updateFingerprint, updateUserId });
@@ -98,63 +69,25 @@ class Main extends Component {
     const { publicKey, fp, id } = this.state;
     return getUserInfo(id, publicKey, fp)
       .then((response) => {
-        // console.log(response, "look here", response.data.refresh_token)
         this.setState({ id: response.data._id, refreshToken: response.data.refresh_token }, () => { this.pushToIframe(response.data.refresh_token); });
-        // const key = response.data.public_key_obj.public_key;
-        // this.setState({ publicKey: key });
-        // return key;
       });
   }
 
-  // createNewUser = (type) => {
-  //   const { props } = this;
-  //   createTestUser()
-  //     .then((response) => {
-  //       // this.generateOauthKey();
-  //       if (type === 'id') {
-  //         return response.data._id;
-  //       }
-  //       props.updateUserInfo('id', response.data._id);
-  //       props.updateUserInfo('refreshToken', response.data.refresh_token);
-  //       this.setState({ id: response.data._id, refreshToken: response.data.refresh_token }, () => this.pushToIframe());
-  //     })
-  //     .catch((err) => {
-  //       switch (err.response.data.http_code) {
-  //         case '500':
-  //           props.updateBanner({
-  //             isOpen: true,
-  //             content: 'The server is down, please try again later.'
-  //           });
-  //           break;
-  //         case '429':
-  //           props.updateBanner({
-  //             isOpen: true,
-  //             content: 'The server is busy, please try again later.'
-  //           });
-  //           break;
-  //         case '503':
-  //           props.updateBanner({
-  //             isOpen: true,
-  //             content: 'The server is busy, please try again later.'
-  //           });
-  //           break;
-  //         case '504':
-  //           props.updateBanner({
-  //             isOpen: true,
-  //             content: 'The server is busy, please try again later.'
-  //           });
-  //           break;
-  //         default:
-  //           props.updateBanner({
-  //             isOpen: true,
-  //             content: 'The server is busy, please try again later.'
-  //           });
-  //       }
-  //     });
-  //   // this.setState({ id: '5d116600f12ce3006db1fc26', refreshToken: 'refresh_hWAXl0wsvJmBbe0QVN3E6yLKG7uD9UOIM4cZHxog' }, () => this.pushToIframe());
-  //   // props.updateUserInfo('id', '5d116600f12ce3006db1fc26');
-  //   // props.updateUserInfo('refreshToken', 'refresh_hWAXl0wsvJmBbe0QVN3E6yLKG7uD9UOIM4cZHxog');
-  // }
+  createNewUser = (type) => {
+    const { props } = this;
+    createTestUser()
+      .then((response) => {
+        if (type === 'id') {
+          return response.data._id;
+        }
+        props.updateUserInfo('id', response.data._id);
+        props.updateUserInfo('refreshToken', response.data.refresh_token);
+        this.setState({ id: response.data._id, refreshToken: response.data.refresh_token }, () => this.updateUserInfo());
+      })
+      .catch((err) => {
+        displayErrorBanner(err.response.data);
+      });
+  }
 
   getPublicKey = () => {
     const { props } = this;
@@ -166,25 +99,6 @@ class Main extends Component {
         return key;
       });
   }
-
-  generateOuth = (refreshToken) => {
-    const { props } = this;
-    const { id, publicKey } = this.state;
-    // console.log("khjkhh", refreshToken)
-    return generateOauthKey(id, refreshToken, publicKey)
-      .then((responseSecond) => {
-        // console.log("oauth_res", responseSecond)
-        localStorage.setItem('userId', responseSecond.data.user_id);
-        localStorage.setItem('synapseOauth', responseSecond.data.oauth_key);
-        props.updateUserInfo('oauth_key', responseSecond.data.oauth_key);
-        this.setState({ load: false, isLoading: null });
-        return responseSecond.data.oauth_key;
-      })
-      .catch((err) => {
-        displayErrorBanner(err.response.data);
-      });
-  }
-
 
   render() {
     const { load, isLoading } = this.state;
@@ -200,7 +114,6 @@ class Main extends Component {
               <div className="main-left-child" id="link-button-iframe">
                 <div>
                   <img className="user-gif" src={userDemoGif} alt="gif" />
-                  {/* <Button id="link-button-iframe" className="iframe-btn" type="button">Click for demo </Button> */}
                 </div>
                 <div>
                   <h1 className="userAgentHeader">End User</h1>
--- src/services/nodeService.js
+++ src/services/nodeService.js
@@ -1,39 +1,23 @@
 import axios from 'axios';
 import store from '../store/store';
+import { generateName } from '../constants/userRandomNames';
 
 const URL_USER = '/api/v3.1/users';
-// const oauthKey = 'oauth_snMDtxJzhaLR13BIEAFNSiqjdXkYZ0uvowpyrKC4';
-// const userId = '5cdca3d814ddee0064a05b17';
 const oauthKey = localStorage.getItem('synapseOauth');
 const userId = localStorage.getItem('userId');
 const publicKey = 'public_key_qWCwNJcVPT2jMY105s7K6bUDm3gixoXkf94ZrR8F';
 const ipAddress = '172.69.22.117';
+const clientId = 'client_id_sqkFx4C8Hz0Snaib3eT6oPDUu0K7ytXjAd2pNJWG';
+const clientSecret = 'client_secret_irJqTlDU8NVnEtjMbxmO6uRzYp1f0KCIF70wdS52';
+const fingerPrint = 'badc522c6a325711f51841fc6f1e8bd0';
 
-// localStorage.setItem('userId', '5b5f95dcf122e319740dbc76');
-// localStorage.setItem('synapseOauth', 'oauth_y4NKwCgfj2QF1svkoV5pqHuU7Im8OXaxZTAtd0hL');
-// localStorage.setItem('ipAddress', '172.69.22.117');
-// localStorage.setItem('publicKey', 'public_key_n7YOZjuGemJ9cPWBKQLNMH2h0iaU1X0v5CrdgAIx');
-
-export function fetchNodes(type, id, oauth) {
-  let URL_NODE = `${URL_USER}/${id}/nodes`;
-  if (type) {
-    URL_NODE += `?type=${type}`;
-  }
-  return axios.get(URL_NODE, {
-    headers: {
-      'Content-Type': 'application/json',
-      'X-SP-USER': `${oauth}|`,
-      'X-SP-USER-IP': '172.69.22.117'
-    }
-  });
-}
 
 export function createTestUser() {
   const config = {
     headers: {
       'Content-Type': 'application/json',
-      'X-SP-GATEWAY': 'public_key_qWCwNJcVPT2jMY105s7K6bUDm3gixoXkf94ZrR8F',
-      'X-SP-USER': '|badc522c6a325711f51841fc6f1e8bd0',
+      'X-SP-GATEWAY': `${publicKey}`,
+      'X-SP-USER': `|${fingerPrint}`,
       'X-SP-USER-IP': `${ipAddress}`
     }
   };
@@ -44,10 +28,10 @@ export function createTestUser() {
       }
     ],
     phone_numbers: [
-      '901.111.1111'
+      '202-555-0137', '415-526-8943'
     ],
     legal_names: [
-      'Test Chatbot'
+      generateName()
     ],
     ips: [
       '127.0.0.1'
@@ -60,40 +44,11 @@ export function createTestUser() {
   return axios.post(URL_USER, payload, config);
 }
 
-
-export function generateOauthKey(id, refreshToken, newpublicKey) {
-  const config = {
-    headers: {
-      'Content-Type': 'application/json',
-      // 'X-SP-GATEWAY': 'public_key_qWCwNJcVPT2jMY105s7K6bUDm3gixoXkf94ZrR8F',
-      'X-SP-GATEWAY': `${newpublicKey}`,
-      'X-SP-USER': '|badc522c6a325711f51841fc6f1e8bd0',
-      'X-SP-USER-IP': `${ipAddress}`
-    }
-  };
-  const payload = {
-    refresh_token: `${refreshToken}`,
-    scope: [
-      'USER|PATCH',
-      'USER|GET',
-      'CONVERSATION|GET',
-      'CONVERSATION|POST',
-      'CONVERSATION|PATCH',
-      'CONVERSATIONS|GET',
-      'CONVERSATIONS|POST',
-      'MESSAGES|POST',
-      'MESSAGES|GET'
-    ]
-  };
-  return axios.post(`https://uat-uiaas-v2.synapsefi.com/api/v3.1/oauth/${id}`, payload, config);
-}
-
-
 export function generatePublicKey(id) {
   const config = {
     headers: {
       'Content-Type': 'application/json',
-      'X-SP-GATEWAY': 'client_id_sqkFx4C8Hz0Snaib3eT6oPDUu0K7ytXjAd2pNJWG|client_secret_irJqTlDU8NVnEtjMbxmO6uRzYp1f0KCIF70wdS52',
+      'X-SP-GATEWAY': `${clientId}|${clientSecret}`,
     }
   };
   return axios.get(`api/v3.1/client?issue_public_key=YES&scope=OAUTH|POST,USERS|POST,USERS|GET,USER|GET,USER|PATCH,SUBSCRIPTIONS|GET,SUBSCRIPTIONS|POST,SUBSCRIPTION|GET,SUBSCRIPTION|PATCH,CLIENT|REPORTS,CLIENT|CONTROLS&user_id=${id}`, config);
